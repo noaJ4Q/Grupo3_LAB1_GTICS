2d067e31758325996f0aaf01b301099280d6e656
noe <noe.jaraq@gmail.com>
2023-03-28 20:55:52 -0500

    New commit

---
+++ Grupo3_LAB1_GTICS/src/main/java/com/example/grupo3_lab1_gtics/Grupo3Lab1GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo3_lab1_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo3Lab1GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo3Lab1GticsApplication.class, args);
+    }
+
+}
---
+++ Grupo3_LAB1_GTICS/src/test/java/com/example/grupo3_lab1_gtics/Grupo3Lab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo3_lab1_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo3Lab1GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
